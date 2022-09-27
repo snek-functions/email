7951c530073477fb282ef3299b8c221ab0b3d76f
Florian Kleber <kleberbaum@erebos.xyz>
2022-09-27 09:21:25 +0000

    feat: add send2fa
     - add 2FATemplate
     - add send2fa function

---
+++ src/send2fa.ts
@@ -0,0 +1,38 @@
+import {fn} from './factory'
+import {Email} from './email/2FATemplate.js'
+
+const send2fa = fn<{email: string; subject: string; firstName: string; lastName: string; link: string}, void>(
+  async ({email, subject, firstName, lastName, link}, _, req) => {
+    const {sendMail} = await import('./internal/email.js')
+
+    // SMTP_HOST=smtp-relay.snek.at
+    // SMTP_PORT=587
+    // SMTP_USER=snekman@snek.at
+    // SMTP_PASSWORD=xxxxxxxxxxxxxxxxxxxxxx
+    // SMTP_SENDER=noreply@snek.at
+
+    const res = await sendMail(
+      {
+        from: `${(process.env.SMTP_SENDER, process.env.SMTP_USER)}`,
+        to: email,
+        subject: subject,
+        html: Email(link, firstName, lastName)
+      },
+      {
+        host: `${process.env.SMTP_HOST}`,
+        port: `${process.env.SMTP_PORT}`,
+        secure: true,
+        user: `${process.env.SMTP_USER}`,
+        password: `${process.env.SMTP_PASSWORD}`
+      }
+    )
+
+    console.log(email)
+    console.log(res)
+  },
+  {
+    name: 'send2fa'
+  }
+)
+
+export default send2fa

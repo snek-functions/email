b5f96756140941cdff883e76d077f2ea57d407a2
Florian Kleber <kleberbaum@erebos.xyz>
2022-10-20 17:29:32 +0000

    fix: add sendPasswordReset to index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export {default as send} from './send.js'
-export {default as send2fa} from './send2fa.js'
+export { default as send } from './send.js'
+export { default as send2fa } from './send2fa.js'
+export { default as sendPasswordReset } from './sendPasswordReset.js'
 
 // SPDX-License-Identifier: (EUPL-1.2)
 // Copyright © 2019-2022 snek.at
